3910fbbb85f9506bfe7c7a177fb87deee326ce26
tomasf71 <friml.tomas@icloud.com>
2017-07-23 09:36:30 +0200

    no message

--- examples/EncoderTest/EncoderTest.ino
+++ examples/EncoderTest/EncoderTest.ino
@@ -1,46 +1,67 @@
 
-volatile boolean TurnDetected;
-volatile boolean up;
+#include <Arduino.h>
 
-const int PinCLK=2;                   // Used for generating interrupts using CLK signal
-const int PinDT=3;                    // Used for reading DT signal
-const int PinSW=4;                    // Used for the push button switch
+// -----------------------------------------------------------------------------
+// constants
 
-void isr ()  {                    // Interrupt service routine is executed when a HIGH to LOW transition is detected on CLK
- if (digitalRead(PinCLK))
-   up = digitalRead(PinDT);
- else
-   up = !digitalRead(PinDT);
- TurnDetected = true;
-}
+const int                                 PinCLK   = 2;     // Used for generating interrupts using CLK signal
+const int                                 PinDT    = 4;     // Used for reading DT signal
+const int                                 PinSW    = 8;     // Used for the push button switch
 
+// -----------------------------------------------------------------------------
+// global vars
 
-void setup ()  {
- pinMode(PinCLK,INPUT);
- pinMode(PinDT,INPUT);  
- pinMode(PinSW,INPUT);
- attachInterrupt (0,isr,FALLING);   // interrupt 0 is always connected to pin 2 on Arduino UNO
- Serial.begin (9600);
- Serial.println("Start");
-}
+volatile int                             virtualPosition    = 0;
 
-void loop ()  {
- static long virtualPosition=0;    // without STATIC it does not count correctly!!!
+// -----------------------------------------------------------------------------
+// forward decls
 
- if (!(digitalRead(PinSW))) {      // check if pushbutton is pressed
-   virtualPosition=0;              // if YES, then reset counter to ZERO
-   Serial.print ("Reset = ");      // Using the word RESET instead of COUNT here to find out a buggy encoder
-   
-   Serial.println (virtualPosition);
- }  
- 
- if (TurnDetected)  {        // do this only if rotation was detected
-   if (up)
-     virtualPosition++;
-   else
-     virtualPosition--;
-   TurnDetected = false;          // do NOT repeat IF loop until new rotation detected
-   Serial.print ("Count = ");  
-   Serial.println (virtualPosition);
- }
-}
+void isr();
+void loop();
+void setup();
+
+// -----------------------------------------------------------------------------
+// Interrupt service routine is executed when a HIGH to LOW transition is detected on CLK
+
+void isr ()  {
+    if (!digitalRead(PinDT))
+        virtualPosition = virtualPosition + 1;
+    else
+        virtualPosition = virtualPosition - 1;
+    } // isr
+
+// -----------------------------------------------------------------------------
+
+void setup() {
+    Serial.begin(9600);
+
+    pinMode(PinCLK,INPUT);
+    pinMode(PinDT, INPUT);
+    pinMode(PinSW, INPUT_PULLUP);
+
+    attachInterrupt(digitalPinToInterrupt(PinCLK), isr, FALLING);   // interrupt 0 is always connected to pin 2 on Arduino UNO
+
+    Serial.println("Start");
+
+    } // setup
+
+// -----------------------------------------------------------------------------
+
+void loop() {
+
+    int                                    lastCount = 0;
+
+    while (true) {
+        if (!(digitalRead(PinSW))) {        // check if pushbutton is pressed
+            virtualPosition = 0;            // if YES, then reset counter to ZERO
+            while (!digitalRead(PinSW)) {}  // wait til switch is released
+            delay(10);                      // debounce
+            Serial.println("Reset");        // Using the word RESET instead of COUNT here to find out a buggy encoder
+            }
+        if (virtualPosition != lastCount) {
+            lastCount = virtualPosition;
+            Serial.print("Count");
+            Serial.println(virtualPosition);
+            }
+        } // while
+    } //loop
